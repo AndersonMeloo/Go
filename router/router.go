3e819f18260578aa5c8107a2f668c11803151cbb
Anderson Melo <142186594+AndersonMeloo@users.noreply.github.com>
2026-04-27 23:51:29 -0300

    feat(rotuer.go) Separando rotas em arquivos - router.go importa as rotas definidas em routes.go

--- router/router.go
+++ router/router.go
@@ -4,11 +4,12 @@ import "github.com/gin-gonic/gin"
 
 func Initialize() {
 
+	// Initialize Router
 	router := gin.Default()
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+
+	// Initialize Routes
+	initializeRoutes(router)
+
+	// Run the server
 	router.Run(":8080") // listens on 0.0.0.0:8080 by default
 }
