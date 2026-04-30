d9a31d881f63a4a4372d28f49ba180fafdf5b653
Anderson Melo <142186594+AndersonMeloo@users.noreply.github.com>
2026-04-29 22:25:39 -0300

    feat: add custom logger

--- main.go
+++ main.go
@@ -1,8 +1,23 @@
 package main
 
-import "github.com/AndersonMeloo/Go.git/router"
+import (
+	"github.com/AndersonMeloo/Go.git/config"
+	"github.com/AndersonMeloo/Go.git/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+
+	// Initialize Configs
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		return
+	}
 
 	router.Initialize()
 }
