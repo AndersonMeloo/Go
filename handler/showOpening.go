a98b854202a210b11091f020fefaaf8ed48f1fe2
Anderson Melo <142186594+AndersonMeloo@users.noreply.github.com>
2026-05-09 19:05:29 -0300

    feat: Create Update Opening type UpdateOpeningRequest struct func Validate UpdateOpeningRequest in UpdateOpeningHandler

--- handler/showOpening.go
+++ handler/showOpening.go
@@ -3,12 +3,22 @@ package handler
 import (
 	"net/http"
 
+	"github.com/AndersonMeloo/Go.git/schemas"
 	"github.com/gin-gonic/gin"
 )
 
 func ShowOpeningHandler(ctx *gin.Context) {
 
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "GET Opening!",
-	})
+	id := ctx.Query("id")
+	if id == "" {
+		sendError(ctx, http.StatusBadRequest, errParamIsRequired("id", "qyueryParameter").Error())
+	}
+
+	opening := schemas.Opening{}
+	if err := db.First(&opening, id).Error; err != nil {
+		sendError(ctx, http.StatusNotFound, "opening not found")
+		return
+	}
+
+	sendSucces(ctx, "show-opening", opening)
 }
